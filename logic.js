e7283b1ce41d8369108c976b9c4fdae182a6ee2f
fuqingren <48401927+fuqingren@users.noreply.github.com>
2019-04-09 20:15:06 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,56 +1,18 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/* global getAssetRegistry getFactory */
-
 /**
+ * Track the trade of a commodity from one trader to another
+ * @param {org.fordham.education.PublishTranscript} publish - the trade to be processed
+ * @transaction
+ */
+async function publishTr(publish){
+    const registry=await getAssetRegistry('org.fordham.education.TranscriptViewBy');
+    const factory=getFactory();
+    //create the asset TranscriptViewBy
 
-* Issue a new transcript
-@param {org.fordham.education.PublishTranscript} publish transcript
-* @transaction
-*/
+    const newasset=factory.newResource('org.fordham.education','TranscriptViewBy',publish.tVid);
+    newasset.status=publish.status;
+    newasset.ReviewedCompany=publish.ReviewedCompany
+  	newasset.company=publish.company
 
-async function publish(publishTranscript) {// eslint-disable-line no-unused-vars
-  const registry =await getAssetRegistry('org.fordham.education.Transcript');
-  const factory = getFactory();
-
-  //create tbe transcript asset.
-  const Transcript=factory.newResource('org.fordham.education', 'Transcript', publishTranscript.TId);
-  Transcript.issueDate=publishTranscript.issueDate;
-  Transcript.gpa=publishTranscript.gpa;
-  Transcript.classOfYear=publishTranscript.classOfYear;
-  Transcript.studentName=publishTranscript.studentName;
-  Transcript.TId=publishTranscript.TId;
-  Transcript.description=publishTranscript.description;
-  Transcript.issuer=publishTranscript.issuer;
-   // Add the bond asset to the registry.
-  await registry.add(Transcript);
+    //add TranscriptViewBy to the registry
+    await registry.add(newasset);
 }
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
