584e7ac882648e4c9ce93354d3f65081ae5381e5
fuqingren <48401927+fuqingren@users.noreply.github.com>
2019-04-11 16:03:40 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,18 +1,33 @@
 /**
  * Track the trade of a commodity from one trader to another
- * @param {org.fordham.education.PublishTranscript} publish - the trade to be processed
+ * @param {org.fordham.education.ModifyTranscript} modify - the trade to be processed
  * @transaction
  */
-async function publishTr(publish){
-    const registry=await getAssetRegistry('org.fordham.education.TranscriptViewBy');
+
+async function modifyTranscript(modify){
+    const Assetregistry=await getAssetRegistry('org.fordham.education.Transcript');
+    //const participantRegistryS = await getParticipantRegistry('org.fordham.education'+'Student');
+    // const participantRegistryC = await getParticipantRegistry('org.fordham.education.Company');
+    // const participantRegistrySC = await getParticipantRegistry('org.fordham.education.School'); // eslint-disable-line no-undef
     const factory=getFactory();
-    //create the asset TranscriptViewBy
+    const newasset=factory.newResource('org.fordham.education','Transcript',modify.mId);
+    //modify aspects
+    newasset.gpa=modify.gpa;
+    newasset.remarks=modify.remarks;
+    newasset.studentName=modify.studentName;
+    newasset.studentId=modify.studentId;
+    newasset.GraduationDate=modify.GraduationDate;
+    newasset.issueDate=modify.issueDate;
+    //participants
+    const SchoolRef=factory.newRelationship('org.fordham.education','School',getCurrentParticipant().getIdentifier());
+    newasset.school=SchoolRef; 
+    //const toStudent = await participantRegistryS.get(modify.student);
+    const StudentRef=factory.newRelationship('org.fordham.education','Student',getCurrentParticipant().getIdentifier());
+    newasset.student=StudentRef; 
+  	const CompanyRef=factory.newRelationship('org.fordham.education','Company',getCurrentParticipant().getIdentifier());
+    newasset.viewedBy=CompanyRef; 
 
-    const newasset=factory.newResource('org.fordham.education','TranscriptViewBy',publish.tVid);
-    newasset.status=publish.status;
-    newasset.ReviewedCompany=publish.ReviewedCompany
-  	newasset.company=publish.company
+  await Assetregistry.add(newasset);
+  
 
-    //add TranscriptViewBy to the registry
-    await registry.add(newasset);
 }
