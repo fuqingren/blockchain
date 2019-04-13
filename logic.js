afb86c68d56edd622762ed96e79a22574b5a40d6
fuqingren <48401927+fuqingren@users.noreply.github.com>
2019-04-13 14:19:09 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -6,9 +6,8 @@
 
 async function modifyTranscript(modify){
     const Assetregistry=await getAssetRegistry('org.fordham.education.Transcript');
-    //const participantRegistryS = await getParticipantRegistry('org.fordham.education'+'Student');
     // const participantRegistryC = await getParticipantRegistry('org.fordham.education.Company');
-    // const participantRegistrySC = await getParticipantRegistry('org.fordham.education.School'); // eslint-disable-line no-undef
+    //const participantRegistryS = await getParticipantRegistry('org.fordham.education.Student'); // eslint-disable-line no-undef
     const factory=getFactory();
     const newasset=factory.newResource('org.fordham.education','Transcript',modify.mId);
     //modify aspects
@@ -18,16 +17,65 @@ async function modifyTranscript(modify){
     newasset.studentId=modify.studentId;
     newasset.GraduationDate=modify.GraduationDate;
     newasset.issueDate=modify.issueDate;
+  	newasset.createdSchool="Fordham University"
     //participants
-    const SchoolRef=factory.newRelationship('org.fordham.education','School',getCurrentParticipant().getIdentifier());
+    const SchoolRef=factory.newRelationship('org.fordham.education','School',getCurrentParticipant().getIdentifier());//add school
     newasset.school=SchoolRef; 
-    //const toStudent = await participantRegistryS.get(modify.student);
-    const StudentRef=factory.newRelationship('org.fordham.education','Student',getCurrentParticipant().getIdentifier());
-    newasset.student=StudentRef; 
+  	//const participantRegistryS = await getParticipantRegistry('org.fordham.education.Student');//get student participant
+    //const toStudent = await participantRegistryS.get(modify.Student);
+    newasset.student=factory.newRelationship('org.fordham.education','Student', getCurrentParticipant().getIdentifier());
+    //newasset.student=StudentRef; 
   	const CompanyRef=factory.newRelationship('org.fordham.education','Company',getCurrentParticipant().getIdentifier());
     newasset.viewedBy=CompanyRef; 
 
   await Assetregistry.add(newasset);
-  
 
 }
+
+/**
+ * Track the trade of a commodity from one trader to another
+ * @param {org.fordham.education.ModifyByStudent} change - the trade to be processed
+ * @transaction
+ */
+
+async function modifyBy(change){
+  //todo list student can add company that he wants to see in the list as well as  removing company
+  // create an array?
+  
+  console.log('student changing company');
+  const factory = getFactory();
+  
+  change.student.company.push(change.addCompany);
+  
+
+  
+  const index=change.student.company.indexOf(change.removeCompany);
+  if (index>-1){
+    change.student.company.splice(index,1);
+   }else  if (change.student.company !== change.newViewBy){
+    throw new Error('Please delete the companies that are on the list')
+  }
+  	
+  
+  
+  
+  const participantRegistry=await getParticipantRegistry('org.fordham.education.Student');
+  await participantRegistry.update(change.student);
+  
+  
+  
+  
+
+
+  
+  
+  
+  
+
+
+}
+
+
+
+
+
